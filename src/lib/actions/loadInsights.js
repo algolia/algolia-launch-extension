7d730bc53a385c005aac80256a86d9d3bb17596c
smomin <sajid.momin@algolia.com>
2025-07-24 22:30:31 -0700

    chore: release v2.3.0

--- src/lib/actions/loadInsights.js
+++ src/lib/actions/loadInsights.js
@@ -31,7 +31,7 @@ module.exports = function(settings) {
     userHasOptedOut: (userHasOptedOutDataElement === null || userHasOptedOutDataElement === '') ? false: userHasOptedOutDataElement,
     cookieDuration: cookieDuration
   });
-  window.aa('addAlgoliaAgent', 'algolia-launch-extension (2.3.0-beta.8)');
+  window.aa('addAlgoliaAgent', 'algolia-launch-extension (2.3.0)');
 
   if (userTokenDataElement) {
     window.aa('setUserToken', userTokenDataElement);
